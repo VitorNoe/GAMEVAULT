b6c51371d2df60b56439def1887e46770e73e429
Vítor Luciano Cardoso Noé <162385647+VitorNoe@users.noreply.github.com>
2025-12-10 19:35:55 -0300

    Implement ApiService with axios interceptors

---
+++ frontend-web/src/services/api.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosInstance, AxiosError } from 'axios';
+import { API_BASE_URL } from '../utils/constants';
+
+class ApiService {
+  private api: AxiosInstance;
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: API_BASE_URL,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    // Request interceptor - add auth token
+    this.api.interceptors.request.use(
+      (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+
+    // Response interceptor - handle errors
+    this.api.interceptors.response.use(
+      (response) => response,
+      (error: AxiosError) => {
+        if (error.response?.status === 401) {
+          localStorage.removeItem('token');
+          window.location.href = '/login';
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  public getApi() {
+    return this.api;
+  }
+}
+
+export const apiService = new ApiService();
+export const api = apiService.getApi();
